eb9365faa59f0e5f3ad4483f1e7fc0e3a09367c1
seonhoshin <sunlike0508@gmail.com>
2024-09-17 20:29:45 +0900

    초기화

---
+++ src/main/java/hello/typeconverter/SpringConverterApplication.java
@@ -0,0 +1,13 @@
+package hello.typeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConverterApplication.class, args);
+    }
+
+}
